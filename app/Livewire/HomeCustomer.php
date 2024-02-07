3801cdfa55081fe9f2b27ea0e4c0e685000da4e7
tecrodrigocastro <tec.rodrigocastro@gmail.com>
2024-02-07 17:30:29 -0300

    home livewire

--- app/Livewire/HomeCustomer.php
+++ app/Livewire/HomeCustomer.php
@@ -6,6 +6,14 @@ use Livewire\Component;
 
 class HomeCustomer extends Component
 {
+
+    public $products = null;
+
+
+    public function mount() {
+        $this->products = \App\Models\Product::all();
+    }
+
     public function render()
     {
         return view('livewire.home-customer');
--- resources/views/livewire/home-customer.blade.php
+++ resources/views/livewire/home-customer.blade.php
@@ -4,20 +4,24 @@
         <!-- HEADER -->
         <div class="flex flex-row justify-between">
             <div class="flex flex-col">
-                <img src="{{asset('images/logovertical.png')}}" alt="" class="w-36">
+                <img src="{{ asset('images/logovertical.png') }}" alt="" class="w-36">
                 <hr class=" w-24 h-0.5 bg-pink-600">
             </div>
 
 
             <div class="flex flex-row gap-2">
-                <a href="{{route('cart')}}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#CB3C68" class="bi bi-cart-fill" viewBox="0 0 16 16">
-                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+                <a href="{{ route('cart') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#CB3C68"
+                        class="bi bi-cart-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
                     </svg>
                 </a>
                 <a href="">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#CB3C68" class="bi bi-person" viewBox="0 0 16 16">
-                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#CB3C68"
+                        class="bi bi-person" viewBox="0 0 16 16">
+                        <path
+                            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
                     </svg>
                 </a>
             </div>
@@ -32,22 +36,25 @@
             </div>
 
             <div>
-                <input type="text" class="w-96 h-10 border-2 border-pink-400 rounded-md placeholder:font-bold placeholder:px-3 px-3" placeholder="O que você está procurando?">
+                <input type="text"
+                    class="w-96 h-10 border-2 border-pink-400 rounded-md placeholder:font-bold placeholder:px-3 px-3"
+                    placeholder="O que você está procurando?">
             </div>
         </div>
         <!-- PRODUCTS -->
         <div class="grid grid-cols-5 gap-9">
-            @for($x= 0; $x <= 20; $x++)
-                <a href="{{route('product', $x)}}">
+            @foreach ($products as $product)
+                <a href="{{ route('product', $product->id) }}">
                     <div class="flex flex-col items-center">
-                        <img src="{{asset('images/bolo.png')}}" alt="" class="bg-cover">
-                        <div class="h-28 w-full bg-gray-100 border border-pink-200 rounded-md text-center flex flex-col justify-around">
-                            <h1 class="text-pink-500 font-bold">Bolo de chocolate</h1>
-                            <p class="text-pink-700 font-bold">R$ 20,00</p>
+                        <img src="{{ asset('storage/{{$product->images[0]}}') }}" alt="" class="bg-cover">
+                        <div
+                            class="h-28 w-full bg-gray-100 border border-pink-200 rounded-md text-center flex flex-col justify-around">
+                            <h1 class="text-pink-500 font-bold">{{ $product->name }}</h1>
+                            <p class="text-pink-700 font-bold">R$ {{$product->price}}</p>
                         </div>
                     </div>
                 </a>
-            @endfor
+            @endforeach
         </div>
 
     </div>
@@ -56,7 +63,7 @@
 
         <div class="flex flex-row justify-center gap-10 ">
             <div class="flex flex-col items-center">
-                <img src="{{asset('images/logovertical.png')}}" alt="" class="w-36">
+                <img src="{{ asset('images/logovertical.png') }}" alt="" class="w-36">
                 <div class="flex flex-row gap-2 text-pink-500 font-bold  ">
                     <a href="" class="">Inicio</a>
                     <a href="" class="">Carrinho</a>
@@ -68,9 +75,11 @@
 
     </div>
 
-<!-- FOOTER -->
+    <!-- FOOTER -->
     <div class="h-auto w-full justify-center flex flex-col text-center bg-pink-600 text-white font-bold">
         <h1>@ 2024 May Cake - Todos os direitos reservados.</h1>
-        <p><\> Desenvolvido por REDRODRIGO</p>
+        <p>
+            <\> Desenvolvido por REDRODRIGO
+        </p>
     </div>
 </div>
